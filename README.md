f6ec50930479fa5de82391e7c7c33617d8b42415
Taylan Pince <taylanpince@gmail.com>
2024-06-23 22:52:44 +0200

    Make stories public and allow anyone to add chapter

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Generative Humans: A Conference Game
 Built for This Next Thing 2024, Generative Humans is a storytelling game that allows a group of strangers to "generate" a story by taking turns. It's meant to inspire creativity and shared storytelling while also riffing on the idea of how LLMs generate stories and writing.
+
+## Installation
+This is a Django project and it's completely Dockerized. You can get a local version running using `docker-compose`:
+
+```
+docker-compose up -d
+```
+
+And you can access the running shell as so:
+
+```
+docker-compose exec web bash
+```
+
+## Deployment
+Project also contains a complete Terraform configuration for AWS deployment. To apply it:
+
+```
+cd terraform
+terraform apply
+```
+
+Terraform will ask for a database password, which you can set during the first run.
--- game/views.py
+++ game/views.py
@@ -25,29 +25,6 @@ class HumanRegisterView(HumanAuthenticatedCheckMixin, View):
             human = form.save()
             login_human(request, human)
 
-            story = Story.objects.filter(
-                is_full=False
-            ).annotate(
-                chapter_count=Count('chapters')
-            ).order_by('chapter_count').first()
-
-            if story:
-                Chapter.objects.create(
-                    story=story, 
-                    human=human, 
-                    index=story.total_chapters + 1
-                )
-
-                if story.total_chapters >= 5:
-                    story.is_full = True
-                    story.save()
-            else:
-                mail_admins(
-                    'Generative Humans: No more stories available',
-                    'No more stories are available for new humans.',
-                    fail_silently=True
-                )
-
             return redirect('game:story_list')
 
         return render(request, self.template_name, {'form': form})
@@ -109,16 +86,16 @@ class HumanLogoutView(View):
         return redirect('game:login')
 
 
-class StoryListView(HumanRequiredMixin, View):
+class StoryListView(View):
     template_name = 'story_list.html'
 
     def get(self, request):
-        chapters = request.human.chapters.all()
+        stories = Story.objects.all().annotate(chapters_count=Count('chapters'))
 
-        return render(request, self.template_name , {'chapters': chapters})
+        return render(request, self.template_name , {'stories': stories})
 
 
-class StoryDetailView(HumanRequiredMixin, View):
+class StoryDetailView(View):
     template_name = 'story_detail.html'
 
     def get(self, request, story_id):
@@ -133,59 +110,31 @@ class ChapterWriteView(HumanRequiredMixin, View):
 
     def get(self, request, story_id):
         story = get_object_or_404(Story, pk=story_id)
-        chapter = story.next_chapter
-
-        if not chapter:
-            return redirect('game:story_list')
-
-        if chapter.human != request.human:
-            return redirect('game:story_list')
-
         form = self.form_class()
+
         return render(request, self.template_name, {
             'form': form,
             'story': story,
-            'chapter': chapter,
         })
 
     def post(self, request, story_id):
         story = get_object_or_404(Story, pk=story_id)
-        chapter = story.next_chapter
-
-        if not chapter:
-            return redirect('game:story_list')
-
-        if chapter.human != request.human:
-            return redirect('game:story_list')
-
         form = self.form_class(request.POST)
 
         if form.is_valid():
             content = form.cleaned_data['content']
-            chapter.body = content
-            chapter.is_completed = True
-            chapter.save()
-
-            next_human = story.next_human
-
-            if next_human:
-                login_url = request.build_absolute_uri(reverse('game:authenticate', args=[next_human.access_token]))
-                text_mail = render_to_string('chapter_email.txt', {'login_url': login_url, 'human': next_human})
-                html_mail = render_to_string('chapter_email.html', {'login_url': login_url, 'human': next_human})
-
-                send_mail(
-                    'Generative Humans: Write your chapter',
-                    text_mail,
-                    'storybot@generativehumans.org',
-                    [next_human.email],
-                    fail_silently=False,
-                    html_message=html_mail
-                )
+            
+            Chapter.objects.create(
+                index=story.total_chapters + 1,
+                body=content,
+                story=story,
+                human=request.human,
+                is_completed=True
+            )
 
             return redirect('game:story_detail', story_id=story_id)
 
         return render(request, self.template_name, {
             'form': form,
             'story': story,
-            'chapter': chapter,
         })
